749654927c2b1c92c0245b8ffcb19ca4b488956b
Dominic Pace <dompace181@gmail.com>
2017-05-12 15:59:55 -0500

    refactor to work XD

--- main.cpp
+++ main.cpp
@@ -5,20 +5,8 @@
 
 using namespace std;
 
-sf::Font font;
-sf::Text invText;
-void init() {
-  if (!font.loadFromFile("res/SourceCodePro-Black.ttf")) {
-    cout << "Font Load Error\n";
-  }
-}
+void init() {}
 void displayInv() {
-  invText.setFont(font);
-  invText.setString("Hello World");
-  invText.setCharacterSize(24);
-  invText.setColor(sf::Color::Red);
-  invText.setStyle(sf::Text::Bold);
-
   bool showInv = true;
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
   }
@@ -30,11 +18,21 @@ int main(int argc, char* argv[]) {
 
   sf::RenderWindow window(sf::VideoMode(800, 600), "My window",
                           sf::Style::Default, settings);
+  sf::Font font;
+  if (!font.loadFromFile("res/SourceCodePro-Black.ttf")) {
+    cout << "Font Load Error\n";
+  }
   sf::CircleShape circ(50);
   circ.setFillColor(sf::Color(100, 250, 50));
   sf::CircleShape encirc(50);
   encirc.setFillColor(sf::Color(100, 0, 50));
   encirc.setPosition(300, 400);
+  sf::Text invText;
+  invText.setFont(font);
+  invText.setString("Hello World");
+  invText.setCharacterSize(24);
+  invText.setColor(sf::Color::Red);
+  invText.setStyle(sf::Text::Bold);
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
