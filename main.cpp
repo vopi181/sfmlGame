a4b0133c129d7a4cb9cfb5dd99f42a7c5f21ef94
Dominic Pace <dompace181@gmail.com>
2017-05-12 16:03:56 -0500

    refactor inits

--- main.cpp
+++ main.cpp
@@ -5,8 +5,24 @@
 
 using namespace std;
 
-void init() {}
+sf::Font font;
+sf::Text invText;
+sf::CircleShape circ(50);
+void init() {
+  if (!font.loadFromFile("res/SourceCodePro-Black.ttf")) {
+    cout << "Font Load Error\n";
+  }
+}
+void initCirc() {
+  circ.setFillColor(sf::Color(100, 250, 50));
+}
 void displayInv() {
+  invText.setFont(font);
+  invText.setString("Hello World");
+  invText.setCharacterSize(24);
+  invText.setColor(sf::Color::Red);
+  invText.setStyle(sf::Text::Bold);
+
   bool showInv = true;
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
   }
@@ -18,21 +34,15 @@ int main(int argc, char* argv[]) {
 
   sf::RenderWindow window(sf::VideoMode(800, 600), "My window",
                           sf::Style::Default, settings);
-  sf::Font font;
-  if (!font.loadFromFile("res/SourceCodePro-Black.ttf")) {
-    cout << "Font Load Error\n";
-  }
-  sf::CircleShape circ(50);
-  circ.setFillColor(sf::Color(100, 250, 50));
+
+  init();
+  initCirc();
+  displayInv();
+
   sf::CircleShape encirc(50);
+
   encirc.setFillColor(sf::Color(100, 0, 50));
   encirc.setPosition(300, 400);
-  sf::Text invText;
-  invText.setFont(font);
-  invText.setString("Hello World");
-  invText.setCharacterSize(24);
-  invText.setColor(sf::Color::Red);
-  invText.setStyle(sf::Text::Bold);
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
